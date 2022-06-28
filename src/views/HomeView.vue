5b034aba1e4d20a047b2eef51133212fcc3fb135
Alex Meyers <meyersam0427@gmail.com>
2022-06-27 21:20:33 -0500

    Initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    </div>
 </template>
-
+<!-- Hello this is alex meyers -->
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
