e1c4c0d3f7f36872daa686bd458d6ae80bbb53f5
Brian Hasal <brian.hasal@gmail.com>
2022-06-28 18:49:02 -0700

    Added Login, Logout, and Create Pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/logout">Logout</router-link> |
+    <router-link to="/resumes/new">Create New Resume</router-link> |
+
   </nav>
   <router-view/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from "../views/LoginView.vue"
+import LogoutView from "../views/LogoutView.vue"
+import ResumesNew from "../views/ResumesNew.vue"
 
 const routes = [
   {
@@ -14,6 +17,21 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  { 
+    path: "/login", 
+    name: "login", 
+    component: LoginView 
+  },
+  { 
+    path: "/logout", 
+    name: "logout", 
+    component: LogoutView 
+  },
+  { 
+    path: "/resumes/new", 
+    name: "resumes-new", 
+    component: ResumesNew 
   }
 ]
 
